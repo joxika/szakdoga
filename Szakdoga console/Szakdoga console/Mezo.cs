97891f397f6295eefe20b4c8edf54575ba038130
joxika <jonatan5885@gmail.com>
2020-02-26 18:26:25 +0100

    Add files via upload

---
+++ Szakdoga console/Szakdoga console/Mezo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Szakdoga_console
+{
+    class Mezo
+    {
+         public int tipus;
+         public string tulajdonos;
+
+        public Mezo(int tipus, string tulajdonos)
+        {
+            this.tipus = tipus;
+            this.tulajdonos = tulajdonos;
+        }
+    }
+}
