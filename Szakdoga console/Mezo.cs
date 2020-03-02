b0ce3441f25142f692a9f4ec4200bb4e57b01fd2
joxika <jonatan5885@gmail.com>
2020-03-02 19:38:42 +0100

    Add files via upload

--- Szakdoga console/Mezo.cs
+++ Szakdoga console/Mezo.cs
@@ -6,13 +6,15 @@ namespace Szakdoga_console
 {
     class Mezo
     {
-         public int tipus;
-         public string tulajdonos;
+        public int tipus;
+        public string tulajdonos;
+        public bool idelepes;
 
-        public Mezo(int tipus, string tulajdonos)
+        public Mezo(int tipus, string tulajdonos, bool idelepes)
         {
             this.tipus = tipus;
             this.tulajdonos = tulajdonos;
+            this.idelepes = idelepes;
         }
     }
 }
--- Szakdoga console/Program.cs
+++ Szakdoga console/Program.cs
@@ -9,70 +9,85 @@ namespace Szakdoga_console
         public readonly int URESMEZO = 0;
         public readonly int GYALOG = 1;
         public readonly int BASTYA = 2;
-        public readonly int LEPHET = 3;
 
-        public static readonly string FEKETE = "fekete";
-        public static readonly string FEHER = "feher";
+        public static readonly string KEK = "kek";
+        public static readonly string PIROS = "piros";
         public static readonly string URES = "ures";
 
-        public string kinekakore = FEHER;
+        public static readonly bool NEMLEHETIDELEPNI = false;
+        public static readonly bool LEHETIDELEPNI = true;
 
-        public int Sor = 5;
-        public int Oszlop = 1;
+        public string kinekakore = PIROS;
 
+        public int Kijeloltsor = 0;
+        public int Kijeloltoszlop = 0;
+
+        public int Hovasor = 2;
+        public int Hovaoszlop = 2;
 
         static void Main(string[] args)
         {
             var program = new Program();
 
+            program.Palyafeltoltes();
             while (true)
             {
-                program.Palyafeltoltes();
                 program.Palyakirajzolas();
                 program.Babukijeloles();
-                Console.Clear();
+                program.Hovalephet();
+                program.Palyakirajzolas();
+                program.Lepes();
             }
         }
 
         void Palyafeltoltes()
         {
             Urespalyafeltoltes();
-            Babuhozzaad(0, 8, BASTYA, FEKETE);
-            Babuhozzaad(8, 16, GYALOG, FEKETE);
-            Babuhozzaad(48, 56, GYALOG, FEHER);
-            Babuhozzaad(56, 64, BASTYA, FEHER);
-        }
+            Babuhozzaad(0, 8, BASTYA, KEK, NEMLEHETIDELEPNI);
+            Babuhozzaad(8, 16, GYALOG, KEK, NEMLEHETIDELEPNI);
+            Babuhozzaad(48, 56, GYALOG, PIROS, NEMLEHETIDELEPNI);
+            Babuhozzaad(56, 64, BASTYA, PIROS, NEMLEHETIDELEPNI);
+        } 
         void Urespalyafeltoltes()
         {
             for (int i = 0; i < 64; i++)
             {
-                Palya.Add(new Mezo(URESMEZO, URES));
+                Palya.Add(new Mezo(URESMEZO, URES, NEMLEHETIDELEPNI));
             }
         }
-        void Babuhozzaad(int mettol, int meddig, int mit, string tulajdonos)
+        void Babuhozzaad(int mettol, int meddig, int mit, string tulajdonos, bool idelepes)
         {
             for (int i = mettol; i < meddig; i++)
             {
                 Palya[i].tipus = mit;
                 Palya[i].tulajdonos = tulajdonos;
-
+                Palya[i].idelepes = idelepes;
             }
         }
 
         void Palyakirajzolas()
         {
-            Console.WriteLine("{0}-nek a köre van", kinekakore);
+            Console.Clear();
+            if(kinekakore == PIROS) 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else 
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            Console.WriteLine("{0} köre van", kinekakore);
             Console.WriteLine();
 
-            for (int oszlop = 0; oszlop < 8; oszlop++)
-            {
-                for (int sor = 0; sor < 8; sor++)
+            for (int sor = 0; sor < 8; sor++)
                 {
-                    Mezo jelenlegimezo = Palya[oszlop * 8 + sor];
+                for(int oszlop = 0; oszlop < 8; oszlop++)
+                {
+                    Mezo jelenlegimezo = Palya[sor * 8 + oszlop];
                     Szinbeallitas(jelenlegimezo.tulajdonos);
 
                     Console.Write(" ");
-                    Hatterszin(sor, oszlop);
+                    Kijeloleshatter(sor, oszlop);
 
                     Console.Write(jelenlegimezo.tipus);
                     Console.BackgroundColor = ConsoleColor.Black;
@@ -86,34 +101,127 @@ namespace Szakdoga_console
             {
                 Console.ForegroundColor = ConsoleColor.Cyan;
             }
-            if (tulajdonos == FEKETE)
+            if (tulajdonos == PIROS)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
             }
-            if (tulajdonos == FEHER)
+            if (tulajdonos == KEK)
             {
                 Console.ForegroundColor = ConsoleColor.Blue;
             }
         }
-        void Hatterszin(int sor, int oszlop)
+        void Kijeloleshatter(int sor, int oszlop)
         {
-            if (Oszlop == oszlop && Sor == sor)
+            if (Kijeloltoszlop == oszlop && Kijeloltsor == sor)
             {
-                Console.BackgroundColor = ConsoleColor.Green;
+                Console.BackgroundColor = ConsoleColor.White;
             }
-            if (Oszlop == oszlop - 1 && Sor == sor || Oszlop == oszlop - 1 && Sor == sor + 1 || Oszlop == oszlop - 1 && Sor == sor - 1 || Oszlop == oszlop && Sor == sor - 1 || Oszlop == oszlop && Sor == sor + 1 || Oszlop == oszlop + 1 && Sor == sor - 1 || Oszlop == oszlop + 1 && Sor == sor || Oszlop == oszlop + 1 && Sor == sor + 1)
+            else if (Palya[sor * 8 + oszlop].idelepes == LEHETIDELEPNI)
+            { 
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+            }
+            else
             {
-                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.BackgroundColor = ConsoleColor.Black;
             }
+            
         }
         void Babukijeloles()
         {
-            Console.WriteLine( );
-            Console.Write("sor: ");
-            Sor = Convert.ToInt32(Console.ReadLine());
-            Console.Write("oszlop: ");
-            Oszlop = Convert.ToInt32(Console.ReadLine());
+            int kijeloltsor;
+            int kijeloltoszlop;
+            if (kinekakore == PIROS)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Bábukijelölés");
+            Console.Write("     sor: ");
+            kijeloltsor = Convert.ToInt32(Console.ReadLine()) -1;
+            Console.Write("     oszlop: ");
+            kijeloltoszlop = Convert.ToInt32(Console.ReadLine()) -1 ;
+            if(Palya[kijeloltsor * 8 + kijeloltoszlop].tulajdonos == kinekakore) 
+            {
+                Kijeloltsor = kijeloltsor;
+                Kijeloltoszlop = kijeloltoszlop;
+            }
+        }
+        void Hovalephet()
+        {
+            for (int sor = 0; sor < 8; sor++)
+            {
+                for (int oszlop = 0; oszlop < 8; oszlop++)
+                {
+                    if (Palya[Kijeloltsor * 8 + Kijeloltoszlop].tulajdonos != URES)
+                    {
+                        if (Kijeloltoszlop == oszlop - 1 && Kijeloltsor == sor && Palya[Kijeloltsor * 8 + Kijeloltoszlop + 1].tulajdonos == URES ||
+                            Kijeloltoszlop == oszlop - 1 && Kijeloltsor == sor + 1 && Palya[(Kijeloltsor - 1) * 8 + Kijeloltoszlop + 1].tulajdonos == URES ||
+                            Kijeloltoszlop == oszlop - 1 && Kijeloltsor == sor - 1 && Palya[(Kijeloltsor + 1)* 8 + Kijeloltoszlop + 1].tulajdonos == URES ||
+                            Kijeloltoszlop == oszlop && Kijeloltsor == sor - 1 && Palya[(Kijeloltsor + 1) * 8 + Kijeloltoszlop].tulajdonos == URES ||
+                            Kijeloltoszlop == oszlop && Kijeloltsor == sor + 1 && Palya[(Kijeloltsor - 1) * 8 + Kijeloltoszlop].tulajdonos == URES ||
+                            Kijeloltoszlop == oszlop + 1 && Kijeloltsor == sor - 1 && Palya[(Kijeloltsor + 1) * 8 + Kijeloltoszlop - 1].tulajdonos == URES ||
+                            Kijeloltoszlop == oszlop + 1 && Kijeloltsor == sor && Palya[Kijeloltsor * 8 + Kijeloltoszlop - 1].tulajdonos == URES ||
+                            Kijeloltoszlop == oszlop + 1 && Kijeloltsor == sor + 1 && Palya[(Kijeloltsor - 1) * 8 + Kijeloltoszlop - 1].tulajdonos == URES)
+                        {
 
+                            Palya[sor * 8 + oszlop].idelepes = LEHETIDELEPNI;
+                        }
+                    }
+                }
+            }
+        }
+        void Lepes()
+        {
+            if (Palya[Kijeloltsor * 8 + Kijeloltoszlop].tulajdonos != URES)
+            {
+                Lepesbekeres();
+                if (Palya[Hovasor * 8 + Hovaoszlop].idelepes == LEHETIDELEPNI)
+                {
+                    Palya[Hovasor * 8 + Hovaoszlop].tulajdonos = Palya[Kijeloltsor * 8 + Kijeloltoszlop].tulajdonos;
+                    Palya[Hovasor * 8 + Hovaoszlop].tipus = Palya[Kijeloltsor * 8 + Kijeloltoszlop].tipus;
+
+                    Palya[Kijeloltsor * 8 + Kijeloltoszlop].tulajdonos = URES;
+                    Palya[Kijeloltsor * 8 + Kijeloltoszlop].tipus = URESMEZO;
+
+                    if(kinekakore == PIROS) 
+                    {
+                        kinekakore = KEK;
+                    }
+                    else 
+                    {
+                        kinekakore = PIROS;
+                    }
+                }
+                for (int sor = 0; sor < 8; sor++)
+                {
+                    for (int oszlop = 0; oszlop < 8; oszlop++)
+                    {
+                        Palya[sor * 8 + oszlop].idelepes = NEMLEHETIDELEPNI;
+                    }
+                }
+            }
+
+        }
+        void Lepesbekeres()
+        {
+            if (kinekakore == PIROS)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Lépés");
+            Console.Write("     sor: ");
+            Hovasor = Convert.ToInt32(Console.ReadLine()) - 1;
+            Console.Write("     oszlop: ");
+            Hovaoszlop = Convert.ToInt32(Console.ReadLine()) - 1;
         }
     }
 }
